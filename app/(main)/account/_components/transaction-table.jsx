cd5cc386f3c0366f30edf5b8cab375a50b7d429d
Freakcodes <gabhay408@gmail.com>
2025-11-04 02:06:30 +0530

    add transaction table

--- app/(main)/account/_components/transaction-table.jsx
+++ app/(main)/account/_components/transaction-table.jsx
@@ -1,5 +1,5 @@
-"use client"
-import React from "react";
+"use client";
+import React, { use } from "react";
 import {
   Table,
   TableBody,
@@ -10,10 +10,35 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Checkbox } from "@/components/ui/checkbox";
-import { Trash } from "lucide-react";
+// import { Trash } from "lucide-react";
 import { format } from "date-fns/format";
+import { categoryColors } from "@/lib/data/categories";
+import { Badge } from "@/components/ui/badge";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import { Button } from "@/components/ui/button";
+import { Clock, MoreHorizontal, RefreshCcw } from "lucide-react";
+const RECURRING_INTERVALS = {
+  DAILY: "Daily",
+  WEEKLY: "Weekly",
+  MONTHLY: "Monthly",
+  YEARLY: "Yearly",
+};
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useRouter } from "next/navigation";
 const TransactionTable = ({ transactions }) => {
-    console.log(transactions);
+  const router=useRouter();
   return (
     <div className="container mx-auto">
       {/* Filters */}
@@ -38,22 +63,80 @@ const TransactionTable = ({ transactions }) => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {
-            transactions.map((transaction)=>(
-
-            
+          {transactions.map((transaction) => (
             <TableRow key={transaction.id}>
               <TableCell>
                 <Checkbox />
               </TableCell>
-              <TableCell className="font-medium">{format(new Date(transaction.date),"PP")}</TableCell>
+              <TableCell className="font-medium">
+                {format(new Date(transaction.date), "PP")}
+              </TableCell>
               <TableCell>{transaction.description}</TableCell>
-              <TableCell>{transaction.category}</TableCell>
-              <TableCell className="text-right">{transaction.amount}</TableCell>
-              <TableCell className="text-right">{transaction.isReccuring}</TableCell>
+              <TableCell className="capitalize px-2">
+                <span
+                  style={{
+                    background: categoryColors[transaction.category],
+                    color: "#fff",
+                  }}
+                  className="px-2 py-1 rounded"
+                >
+                  {transaction.category}
+                </span>
+              </TableCell>
+              <TableCell
+                className="text-right"
+                style={{
+                  color: transaction.type === "INCOME" ? "green" : "red",
+                }}
+              >
+                {transaction.type === "INCOME" ? "+" : "-"}$
+                {transaction.amount.toFixed(2)}
+              </TableCell>
+              <TableCell className="text-right">
+                {transaction.isRecurring ? (
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Badge variant="outline">
+                        <RefreshCcw />
+                        {RECURRING_INTERVALS[transaction.recurringInterval]}
+                      </Badge>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>
+                        {format(new Date(transaction.nextRecurringDate), "PP")}
+                      </p>
+                    </TooltipContent>
+                  </Tooltip>
+                ) : (
+                  <Badge variant="outline">
+                    <Clock />
+                    One Time Only
+                  </Badge>
+                )}
+              </TableCell>
+              <TableCell>
+                <DropdownMenu>
+                  <DropdownMenuTrigger>
+                    <Button variant="ghost" size="icon">
+                      <MoreHorizontal />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                   
+                    <DropdownMenuLabel
+                    onClick={
+                      () => router.push(`/account/transactions?edit=${transaction.id}`)
+                    }
+                    >Edit</DropdownMenuLabel>
+                  <DropdownMenuLabel className="text-destructive">
+                    Delete
+                  </DropdownMenuLabel>
+                   
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </TableCell>
             </TableRow>
-            ))
-          }
+          ))}
         </TableBody>
       </Table>
     </div>
