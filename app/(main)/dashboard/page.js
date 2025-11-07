f4fec144f43fa0abc9189ab18f335589e18817bb
Freakcodes <gabhay408@gmail.com>
2025-11-08 02:33:31 +0530

    add monthly budget

--- app/(main)/dashboard/page.js
+++ app/(main)/dashboard/page.js
@@ -1,32 +1,48 @@
-
-
 import { Button } from "@/components/ui/button";
 import CreateAccountDrawer from "@/components/create-account-drawer";
 import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
 import { getUserAccounts } from "@/actions/dashboard";
 import AccountCard from "./_components/account-card";
-const DashboardPage = async() => {
-  const accounts=await getUserAccounts();
-  
-  return (
-  <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+import { getCurrentBudget } from "@/actions/budget";
+import { BudgetProgress } from "./_components/budget-progress";
+const DashboardPage = async () => {
+  const accounts = await getUserAccounts();
+  const defaultAccount = accounts.find((account) => account.isDefault);
+  let budgetData=null;
+  if(defaultAccount){
+    budgetData=await getCurrentBudget(defaultAccount.id);
 
-   <CreateAccountDrawer>
-    <Card>
-      <CardContent className="flex flex-col cursor-pointer text-muted-foreground justify-center items-center">
-          <Plus className="h-10 w-10 mb-2"/>
-          <p className="text-sm font-medium">Add new account</p>
-      </CardContent>
-    </Card>
-  </CreateAccountDrawer>
-  {
-    accounts.length>0 &&
-    accounts?.map((account)=>(
-      <AccountCard key={account.id} account={account}/>
-    ))
   }
-  </div>
+  return (
+    <div className="px-5">
+      {/* Budget Progress */}
+      {
+        defaultAccount &&(
+          <BudgetProgress
+            initialBudget={budgetData.budget}
+            currentExpenses={budgetData?.expenses||0}
+          />
+        )
+      }
+      {/* Overview */}
+
+      {/* Account Grid */}
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <CreateAccountDrawer>
+          <Card>
+            <CardContent className="flex flex-col cursor-pointer text-muted-foreground justify-center items-center">
+              <Plus className="h-10 w-10 mb-2" />
+              <p className="text-sm font-medium">Add new account</p>
+            </CardContent>
+          </Card>
+        </CreateAccountDrawer>
+        {accounts.length > 0 &&
+          accounts?.map((account) => (
+            <AccountCard key={account.id} account={account} />
+          ))}
+      </div>
+    </div>
   );
 };
 
