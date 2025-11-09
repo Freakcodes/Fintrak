7187ef17731e4c3c7613d64afd24f65d969ae342
Freakcodes <gabhay408@gmail.com>
2025-11-10 00:50:10 +0530

    add mono repo for fixing the error

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,31 @@
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "randomuser.me",
-          pathname: "/**",
-        },
-      ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        pathname: "/**",
+      },
+    ],
+  },
+
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
     },
-    experimental:{
-      serverActions:{
-        bodySizeLimit:"5mb"
-      }
+  },
+
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
     }
-  };
-  
-  export default nextConfig;
+    return config
+  }
+};
+
+export default nextConfig;
+
   
