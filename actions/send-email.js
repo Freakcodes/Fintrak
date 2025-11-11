b00d804a3097d37245d0cd6cbacd464dbb7f85b0
Freakcodes <gabhay408@gmail.com>
2025-11-12 01:05:31 +0530

    inngest bug try

--- actions/send-email.js
+++ actions/send-email.js
@@ -3,6 +3,7 @@
 import { Resend } from "resend";
 
 export async function sendEmail({to,subject,react}){
+    console.log('Send email is being called with ',{to,subject});
     const resend=new Resend(process.env.RESEND_API_KEY);
     try {
         const data=await resend.emails.send({
